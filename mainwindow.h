5afd90012121d31f765a719a1c3941120787046a
huda-irs <48699770+huda-irs@users.noreply.github.com>
2021-04-23 15:02:17 -0400

    Add files via upload
    added in needed functions for ghost and pacman,, made maze[][] an external variable so i can be accessed into the pacman_movement function, made pacman_movement function a private slot

--- mainwindow.h
+++ mainwindow.h
@@ -14,10 +14,7 @@
 #include <QTimer>
 #include <QLabel>
 #include "QThread"
-#include <iostream>
-#include <QIODevice>
-#include <QFileSystemWatcher>
-#include <QMessageBox>
+
 #define xaxis 30
 #define yaxis 30
 #define length 12
@@ -27,16 +24,26 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-
+extern char maze[22][37];
 class Character: public QGraphicsPixmapItem {
 public:
-    virtual void up();
-    virtual void down()=0;
-    virtual void left()=0;
-    virtual void right()=0;
-    char direction;
-    int positionX;
-    int positionY;
+    virtual int get_dir()=0;
+    virtual int get_px()=0;
+    virtual int get_py()=0;
+    virtual int get_posx()=0;
+    virtual int get_posy()=0;
+    virtual int get_changept()=0;
+    virtual void set_px(int)=0;
+    virtual void set_py(int)=0;
+    virtual void set_posx(int)=0;
+    virtual void set_posy(int)=0;
+    virtual void set_dir(int)=0;
+    virtual void set_nextdir(int)=0;
+    virtual void set_changept(int)=0;
+    int state, bluetik,begintik;
+    bool throughgate;
+    char dir;
+
     struct animation {
         QPixmap img;
         animation *next;
@@ -46,12 +53,29 @@ public:
 class Pacman: public Character {
 public:
     Pacman(int,int);
-    void up();
-    void down();
-    void left();
-    void right();
+    void moveup();
+    void movedown();
+    void moveleft();
+    void moveright();
+    int get_px(){return px;}
+    int get_py(){return py;}
+    int get_posx(){return posx;}
+    int get_posy(){return posy;}
+    int get_dir(){return dir;}
+    int get_nextdir(){return nextdir;}
+    int get_changept(){return changept;}
+    void set_px(int n){px=n;}
+    void set_py(int n){py=n;}
+    void set_posx(int n){posx=n;}
+    void set_posy(int n){posy=n;}
+    void set_dir(int d){dir = d;}
+    void set_nextdir(int nd){nextdir = nd;}
+    void set_changept(int npt){changept=npt;}
 private:
-    int x, y, navigate;
+    int addscore();
+    int px,py,posx,posy,dir, nextdir,changept;
+    bool inbox;
+    animation *anim[4][10], *anindex[4];
 };
 
 class ghost: public Character {
@@ -65,35 +89,26 @@ private:
 
 };
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     Character *gPac, *ghost[1];
     void build_maze();
-   // void pacman_movement();
+    void score();
+    //void pacman_movement();
+    //void ghost_movement();
     ~MainWindow();
 private slots:
     void pacman_movement();
-    void showModified(const QString& str) {
-        Q_UNUSED(str)
-        QFile movement("/Users/mandyyao/Desktop/read.txt");
-        movement.open(QIODevice::ReadWrite|QIODevice::Text);
-        QTextStream stream(&movement);
-        QByteArray direction = movement.readLine();
-        if (direction != "") {
+    //void ghost_movement();
 
-            char get = direction.at(0); // gets updated text
-            qInfo() << get; // prints updated text
-            // change global variable = huda
-        }
-    }
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
     QTimer *ptik;
-
 };
 
 
